3dddea7388b1f8e608f33f8b028063627446ed0d
bai <baiyshdykanov6@gmail.com>
2022-01-08 22:44:23 +0300

    commit js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,20 @@ const myBurger = document.querySelector('.burger')
 const myMenu = document.querySelector('.menu')
 const menuLinks = document.querySelectorAll('.menu-link')
 
+myBurger.addEventListener('click', () =>{
+    myBurger.classList.toggle('show-menu')
+    document.body.classList.toggle('o-hidden')
+})
 
 myBurger.addEventListener('click', () =>{
     myMenu.classList.toggle('show')
+    document.body.classList.toggle('o-hidden')
 })
 
-menuLinks.forEach(oneLink => {
+menuLinks.forEach( oneLink=> {
     oneLink.addEventListener('click', () => {
+        myMenu.classList.remove('show-menu')
         myMenu.classList.remove('show')
-
+        document.body.classList.toggle('o-hidden')
     })
 })
-
-
